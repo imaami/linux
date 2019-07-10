5ff7e1601d25b04f7e42150585994237fcd78614
Yazen Ghannam <Yazen.Ghannam@amd.com>
2019-07-10 18:37:16 +0000

    acpi/cppc: Add support for optional CPPC registers
    Newer AMD processors support a subset of the optional CPPC registers.
    Add support for these optional registers.
    
    Signed-off-by: Yazen Ghannam <Yazen.Ghannam@amd.com>
    [ carved out into a patch, cleaned up, productized ]
    Signed-off-by: Janakarajan Natarajan <Janakarajan.Natarajan@amd.com>

--- include/acpi/cppc_acpi.h
+++ include/acpi/cppc_acpi.h
@@ -113,6 +113,8 @@ struct cppc_perf_ctrls {
 	u32 max_perf;
 	u32 min_perf;
 	u32 desired_perf;
+	u32 auto_sel_enable;
+	u32 energy_perf;
 };
 
 struct cppc_perf_fb_ctrs {
@@ -136,6 +138,7 @@ struct cppc_cpudata {
 extern int cppc_get_desired_perf(int cpunum, u64 *desired_perf);
 extern int cppc_get_perf_ctrs(int cpu, struct cppc_perf_fb_ctrs *perf_fb_ctrs);
 extern int cppc_set_reg(int cpu, struct cppc_perf_ctrls *perf_ctrls, enum cppc_regs reg_idx);
+extern int cppc_get_perf(int cpu, struct cppc_perf_ctrls *perf_ctrls);
 extern int cppc_get_perf_caps(int cpu, struct cppc_perf_caps *caps);
 extern int acpi_get_psd_map(struct cppc_cpudata **);
 extern unsigned int cppc_get_transition_latency(int cpu);
