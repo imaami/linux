65e80cf18ad9ed67c192d9314737c182c5970449
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2021-11-01 17:32:12 +0200

    cpupower: threading

--- tools/power/cpupower/utils/idle_monitor/mperf_monitor.c
+++ tools/power/cpupower/utils/idle_monitor/mperf_monitor.c
@@ -5,8 +5,11 @@
 
 #if defined(__i386__) || defined(__x86_64__)
 
+#include <errno.h>
 #include <limits.h>
+#include <pthread.h>
 #include <sched.h>
+#include <semaphore.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -18,8 +21,6 @@
 
 #include "helpers/helpers.h"
 #include "idle_monitor/cpupower-monitor.h"
-
-#define PER_CPU_TSC
 
 #define MSR_APERF	0xE8
 #define MSR_MPERF	0xE7
@@ -37,7 +38,6 @@
 #define UL_CPUMASK_LEN(n) (((size_t)(n) + UL_BIT - 1u) / UL_BIT)
 #define UL_CPUMASK(ncpus) struct { unsigned long __bits[UL_CPUMASK_LEN(ncpus)]; }
 
-#define MAX_CPUS 32
 typedef UL_CPUMASK(MAX_CPUS) cpu_af_t;
 
 enum mperf_id { C0 = 0, Cx, AVG_FREQ, MPERF_CSTATE_COUNT };
@@ -108,6 +108,10 @@ struct measurement {
 	bool is_valid;
 };
 
+struct cpu_thread {
+	pthread_t id;
+};
+
 static cpu_af_t cpu_affinity;
 static struct measurement *stats;
 
@@ -124,6 +128,9 @@ static const int cpu_topo[32] = {
 	0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
 #endif
 };
+
+static struct cpu_thread *cpu_thread;
+static sem_t thread_start_sem;
 
 /*
 static __always_inline int mperf_get_tsc(unsigned long long *tsc)
@@ -350,12 +357,15 @@ static int mperf_start_rdpru_cpusched(void)
 	struct timespec ts2 = {0,0};
 */
 
+	for (; tmp.cpu < cpu_count; ++tmp.cpu)
+		sem_post(&thread_start_sem);
+
 	//clock_gettime(CLOCK_MONOTONIC_RAW, &ts1);
 #ifndef PER_CPU_TSC
 	mperf_rdtsc(&tsc1);
 #endif
 
-	for (; tmp.cpu < 32; ++tmp.cpu) {
+	for (tmp.cpu = 0; tmp.cpu < 32; ++tmp.cpu) {
 		mperf_init_stats_rdpru_cpusched(cpu_topo[tmp.cpu]);
 	}
 
@@ -650,6 +660,19 @@ use_sysfs:
 	return 0;
 }
 
+static void *cpu_fn(void *arg)
+{
+	__attribute__((unused)) struct cpu_thread *thd = arg;
+	__attribute__((unused)) intptr_t id = thd - cpu_thread;
+
+	if (sem_wait(&thread_start_sem))
+		return (void *)(intptr_t)errno;
+
+	mperf_init_stats_rdpru_cpusched(cpu_topo[id]);
+
+	return NULL;
+}
+
 /*
  * This monitor provides:
  *
@@ -666,6 +689,14 @@ struct cpuidle_monitor mperf_monitor;
 static struct cpuidle_monitor *mperf_register(void)
 {
 	int i = 0;
+	int thd = 0;
+	int errnum = 0;
+	const char *errmsg = NULL;
+	pthread_attr_t attr;
+	cpu_set_t cpumask;
+
+	if (cpu_count < 1)
+		return NULL;
 
 	if (!(cpupower_cpu_info.caps & (unsigned int)CPUPOWER_CAP_APERF))
 		return NULL;
@@ -673,13 +704,20 @@ static struct cpuidle_monitor *mperf_register(void)
 	if (init_maxfreq_mode())
 		return NULL;
 
+	errnum = pthread_attr_init(&attr);
+	if (errnum) {
+		errmsg = "pthread_attr_init";
+		goto fail;
+	}
+
 	/* Save original CPU affinity mask so we can restore it after each
 	 * measurement loop.
 	 */
 	if (sched_getaffinity(getpid(), sizeof(cpu_affinity),
 			      (cpu_set_t *)&cpu_affinity)) {
-		perror(__func__);
-		return NULL;
+		errnum = errno;
+		errmsg = "sched_getaffinity";
+		goto fail1;
 	}
 
 	/* Clear the affinity bit of the last CPU to measure during each
@@ -689,11 +727,28 @@ static struct cpuidle_monitor *mperf_register(void)
 	 */
 	CPU_CLR_S(cpu_count - 1, sizeof(cpu_affinity), &cpu_affinity);
 
+	if (sem_init(&thread_start_sem, 0, 0)) {
+		errnum = errno;
+		errmsg = "sem_init";
+		goto fail1;
+	}
+
+	cpu_thread = calloc(cpu_count, sizeof(*cpu_thread));
+
+	if (!cpu_thread) {
+		errnum = errno;
+		errmsg = "calloc";
+		goto fail2;
+	}
+
 	/* Free this at program termination */
 	stats = calloc(cpu_count, sizeof(*stats));
 
-	if (!stats)
-		return NULL;
+	if (!stats) {
+		errnum = errno;
+		errmsg = "calloc";
+		goto fail3;
+	}
 
 	for (; i < cpu_count; ++i) {
 		CPU_SET_S(i, sizeof(cpu_af_t), &stats[i].cpu_affinity);
@@ -701,6 +756,27 @@ static struct cpuidle_monitor *mperf_register(void)
 
 	mperf_monitor.flags.per_cpu_schedule = (cpupower_cpu_info.vendor ==
 						X86_VENDOR_AMD);
+
+	CPU_ZERO(&cpumask);
+	for (thd = 0; thd < cpu_count; ++thd) {
+		CPU_SET(thd, &cpumask);
+		errnum = pthread_attr_setaffinity_np(&attr, sizeof(cpumask),
+						     &cpumask);
+		if (errnum) {
+			errmsg = "pthread_attr_setaffinity_np";
+			goto fail4;
+		}
+		errnum = pthread_create(&cpu_thread[thd].id, &attr, cpu_fn,
+					&cpu_thread[thd]);
+		if (errnum) {
+			errmsg = "pthread_create";
+			goto fail4;
+		}
+		CPU_CLR(thd, &cpumask);
+	}
+
+	pthread_attr_destroy(&attr);
+
 /*
 	if (mperf_monitor.flags.per_cpu_schedule) {
 		if (!(cpupower_cpu_info.caps & CPUPOWER_CAP_AMD_RDPRU)) {
@@ -716,6 +792,27 @@ static struct cpuidle_monitor *mperf_register(void)
 	}
 */
 	return &mperf_monitor;
+
+fail4:
+	for (i = 0; i < thd; ++i)
+		pthread_cancel(cpu_thread[i].id);
+	for (i = 0; i < thd; ++i)
+		pthread_join(cpu_thread[i].id, NULL);
+	free(stats);
+	stats = NULL;
+fail3:
+	free(cpu_thread);
+	cpu_thread = NULL;
+fail2:
+	sem_destroy(&thread_start_sem);
+fail1:
+	pthread_attr_destroy(&attr);
+fail:
+	if (errmsg)
+		fprintf(stderr, "%s: %s%s%s\n", __func__, errmsg,
+			errnum ? ": " : "", errnum ? strerror(errnum) : "");
+	return NULL;
+
 }
 
 static void mperf_unregister(void)
