d0dee92457f2b8986694f2e39b7f1c4b518e0a5a
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2022-08-20 00:17:46 +0300

    better diff script

--- drivers/hwmon/fdiff.sh
+++ drivers/hwmon/fdiff.sh
@@ -2,6 +2,23 @@
 
 fmt=(clang-format --style=file:"$(dirname "$0")/../../.clang-format")
 
-diff -u -ZEBbw --color=always \
-	<("${fmt[@]}" < "$1") \
-	<("${fmt[@]}" < "$2") | less -RS
+[[ -f "$1" && -f "$2" ]] &&
+tmp="$(mktemp -d)"       &&
+[[ -d "$tmp" ]]          &&
+{
+	a="a/${1##*/}"
+	b="b/${2##*/}"
+	unset out
+	mkdir -p "$tmp/a" "$tmp/b"     &&
+	"${fmt[@]}" < "$1" > "$tmp/$a" &&
+	"${fmt[@]}" < "$2" > "$tmp/$b" &&
+	pushd "$tmp" >/dev/null 2>&1   &&
+	{
+		out=$(git diff --color=always --no-index --no-prefix   \
+		               --ignore-blank-lines --ignore-cr-at-eol \
+		               --ignore-space-at-eol -b -w -- "$a" "$b")
+		popd >/dev/null 2>&1
+	}
+	rm -fr "$tmp/"
+	[[ "$out" ]] && less -R -S <<< "$out"
+}
