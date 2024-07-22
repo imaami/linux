e02d4f6a6c1eaf3951057171b0abc3304653afbe
黄清榆 <1353446724@qq.com>
2024-07-22 16:15:44 +0800

    Project-C v6.6-lts-r2

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -75,7 +75,7 @@ __read_mostly int sysctl_resched_latency_warn_once = 1;
 #define sched_feat(x)	(0)
 #endif /* CONFIG_SCHED_DEBUG */
 
-#define ALT_SCHED_VERSION "v6.6-lts-r1"
+#define ALT_SCHED_VERSION "v6.6-lts-r2"
 
 /* rt_prio(prio) defined in include/linux/sched/rt.h */
 #define rt_task(p)		rt_prio((p)->prio)
